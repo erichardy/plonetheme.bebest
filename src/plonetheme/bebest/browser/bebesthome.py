8f30012782f67475d8a6e3db1db39acc46794a67
Eric Hardy <Eric.Hardy@univ-brest.fr>
2016-04-19 19:21:46 +0200

    view

--- src/plonetheme/bebest/browser/bebesthome.py
+++ src/plonetheme/bebest/browser/bebesthome.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 
-# import logging
+import logging
 from zope.publisher.browser import BrowserView
 from plone import api
 
+logger = logging.getLogger('bebest')
 
 class bebestHome(BrowserView):
 
@@ -13,12 +14,18 @@ class bebestHome(BrowserView):
 
     def getCarouselImages(self):
         context = self.context
+        if context.portal_type != 'Folder':
+            context = context.aq_parent
         objs = []
-        images = api.content.find(context=context['bebest-carousel'],
+        try:
+            images = api.content.find(context=context['bebest-carousel'],
                                   depth=1,
                                   portal_type='Image')
-        for image in images:
-            objs.append(image.getObject())
+            for image in images:
+                objs.append(image.getObject())
+            logger.info(len(images))
+        except Exception:
+            logger.info('0 images !')
         return objs
 
     def localText(self, obj):
