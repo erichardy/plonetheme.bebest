fc6b8cc4a05ec540491e99b99691f2efaddc18dc
Eric Hardy <Eric.Hardy@univ-brest.fr>
2016-04-26 15:00:51 +0200

    rename misc files & dropdown menu

--- src/plonetheme/bebest/theme/js/bebest-theme.js
+++ src/plonetheme/bebest/theme/js/bebest-theme.js
@@ -4,4 +4,13 @@ $('#home-carousel-bebest').carousel({
   interval: 4000
 });
 */
+/*
+*/
+
+/* code from http://codepen.io/betdream/pen/frDqh */
+$('ul.nav li.dropdown').hover(function() {
+  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+}, function() {
+  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+});
 
