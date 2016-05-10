a14376177bb5e42f53fa77964ccd885490055b2d
Eric Hardy <Eric.Hardy@univ-brest.fr>
2016-05-10 12:34:06 +0200

    map in progress

--- src/plonetheme/bebest/theme/js/bebest-map.js
+++ src/plonetheme/bebest/theme/js/bebest-map.js
@@ -36,7 +36,7 @@ var baseLayers = { //Appel des différents layers
 L.control.layers(baseLayers).addTo(mymap); // Bouton pour permettre au utilisateurs de choisir la map, controle la variable si dessus.
 
 // début des points
-
+/*
 var marker = L.marker([51.5, -0.09], {icon: bebestIcon}).addTo(mymap); //Variable Marker qui fais appel au marker leaflet avec la geolocalisation, il appelle ensuite la variable bebestIcon et ajoute a mymap.
 //données de la popup
 marker.bindPopup("<div id='bebest-popup-content'><h2>IUEM<h2><hr><h3>MISSION XSZ20</h3><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ut feugiat ex. Praesent urna mi, maximus pretium dictum tristique, sodales sed enim.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ut feugiat ex. Praesent urna mi, maximus pretium dictum tristique, sodales sed enim.. <br/><a href='#'>Click to learn more...</a></p></div>").openPopup();    
@@ -49,8 +49,36 @@ marker.bindPopup("<div id='bebest-popup-content'><h2>UQAR<h2><hr><h3>MISSION XSZ
 var marker = L.marker([78.5, 10.75], {icon: bebestIcon}).addTo(mymap); //Variable Marker qui fais appel au marker leaflet avec la geolocalisation, il appelle ensuite la variable bebestIcon et ajoute a mymap.
 //données de la popup
 marker.bindPopup("<div id='bebest-popup-content'><h2>Svalbard<h2><hr><h3>MISSION XSZ22</h3><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ut feugiat ex. Praesent urna mi, maximus pretium dictum tristique, sodales sed enim.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ut feugiat ex. Praesent urna mi, maximus pretium dictum tristique, sodales sed enim.. <br/><a href='#'>Click to learn more...</a></p></div>").openPopup();   
+*/
+L.Icon.Default.imagePath = 'http://api.tiles.mapbox.com/mapbox.js/v1.0.0beta0.0/images';
+var geojsonFeature = {
+	    "type": "Feature",
+	    "properties": {
+	        "name": "Coors Field",
+	        "amenity": "Baseball Stadium",
+	        "popupContent": "This is where the Rockies play!"
+	    },
+	    "geometry": {
+	        // "type": "LineString",
+	        "type": "Point",
+	        // "coordinates": [[-105, 40], [-110, 45], [-115, 55]]
+	        // "coordinates": [[0, 0], [0, 52], [0, 56]]
+	        "coordinates": [-5, 48.35]
+	    }
+	};
+geojsonLayer = L.geoJson(geojsonFeature, {
+    onEachFeature: function (feature, layer) {
+    	layer.on('click', function (e) {
+    		document.getElementById("info").innerHTML = feature.properties.name;
+            $("#feature_infos").stop();
+            $("#feature_infos").fadeIn("fast");
 
-
-
+            console.log(feature.properties.name);
+            $("#feature_infos").fadeOut(5000);
+    	});
+    }
+});
+mymap.addLayer(geojsonLayer);
+// L.geoJson(geojsonFeature).addTo(mymap);
 
 // Voilà un exemple de popup a positionnement absolu qui semble bien marcher: http://jsfiddle.net/expedio/z1nw3pt4/ 
