f1b4e361bb8749316268a4e81189f98b57204d39
Eric Hardy <Eric.Hardy@univ-brest.fr>
2016-06-13 13:40:48 +0200

    test traversing event

--- src/plonetheme/bebest/subscribers.py
+++ src/plonetheme/bebest/subscribers.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import logging
+# from zope.traversing.interfaces import IBeforeTraverseEvent
 # from plone import api
 # from plone.i18n.normalizer.interfaces import INormalizer
 # from zope.component import getUtility
@@ -13,3 +14,10 @@ def setPortraitTitle(context, event):
     context.title = context.family_name + '-' + context.first_name
     # context.id = normalizer.normalize(context.title)
     context.reindexObject()
+
+
+def trav(context, event):
+    logger.info(event.request.keys())
+    logger.info(event.request.get('HTTP_REFERER'))
+    logger.info(event.request.get('ACTUAL_URL'))
+    # import pdb;pdb.set_trace()
