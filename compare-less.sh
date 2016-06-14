9f98c20097ba6716a0c07f8797be8f56319e5827
Eric Hardy <Eric.Hardy@univ-brest.fr>
2016-06-14 09:47:12 +0200

    mv sur images

--- compare-less.sh
+++ compare-less.sh
@@ -2,11 +2,24 @@
 
 if [ $# != 1 ]
 then
-    echo "Parametre !"
+    echo "Nom du fichier LESS en parametre, ou ALL"
     exit
 fi
+
+FILES="bebest-general.less bebest-imports.less bebest-map.less bebest-portfolio.less bebest-theme.less bebest-home.less bebest-layout.less bebest-navbar.less bebest-portrait.less bebest-typo.less"
 
 DEV="/Users/hardy/Dev/plonetheme.bebest/dev/css/"
 THEME="/Users/hardy/Dev/plonetheme.bebest/src/plonetheme/bebest/theme/css/"
 
-diff ${DEV}$1 ${THEME}$1
+if [ $1 = "ALL" ]
+then
+    for FILE in $FILES
+    do
+        echo '------------------------------------------------'
+        echo $FILE
+        diff ${DEV}$FILE ${THEME}$FILE
+    done
+else
+    diff ${DEV}$1 ${THEME}$1
+fi
+
