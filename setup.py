b093728fc2d75ac54562e6d89c66890ccb796807
Sophie Marchand <sophie.marchand@univ-brest.fr>
2017-04-27 14:45:32 +0200

    ajustement texte equipe bebest>apolimer dans portraits

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     classifiers=[
         "Environment :: Web Environment",
         "Framework :: Plone",
-        "Framework :: Plone :: 5.0.3",
+        "Framework :: Plone :: 5.0.6",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
     ],
